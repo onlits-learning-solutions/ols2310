bc1ac23bb2808722153c9457e7f38737cdbfc510
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2023-12-16 19:49:42 +0530

    20231216

--- c/let-us-c/ch-8/ia.c
+++ c/let-us-c/ch-8/ia.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#define SIZE 10
+
 int main(void)
 {
-    
+    int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    int rev[SIZE];
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        rev[i] = arr[SIZE - 1 - i];
+    }
+    printf("The contents of second array are:\n");
+    for (int i = 0; i < SIZE; i++)
+    {
+        printf("%d\n", rev[i]);
+    }
+    return 0;
 }
